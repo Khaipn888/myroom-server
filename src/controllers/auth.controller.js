c244a42ba3fe0201e93804238424a0f9deeb21e9
Khaipn888 <phanngockhai888@gmail.com>
2025-07-05 09:38:51 +0700

    fix set cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -7,14 +7,14 @@ const setAuthCookies = (res, accessToken, refreshToken) => {
   res.cookie("accessToken", accessToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 15 * 60 * 1000, // 15 phút
   });
 
   res.cookie("refreshToken", refreshToken, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 ngày
   });
 };
